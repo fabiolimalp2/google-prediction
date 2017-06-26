f8994cc4370164411bca3308fabdd88299fe533f
fabiolimalp2 <fabiolimatrabalholp2@gmail.com>
2017-06-26 01:19:40 -0300

    Adicionado menu_view

--- application/views/menu_view.php
+++ application/views/menu_view.php
@@ -3,16 +3,79 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 <div class="container">
-  <ul>
-  <li><a href=""> Modelos Enviados Google Storage</a> </li>
-    <li> <a href="base/coyote_status">Status do treinamento do Model</a></li>
-    <li><a href="base/coyote_predict">Usar Predict do Model</a></li>
-    <li><a href="base/coyote_upload">Enviar Model para treinar </a></li>
-
-    <li><a href=""> Modelos Hospedados do Google</a> </li>
-
-    <li><a href="base/category"> Modelos Hospedados do Google Categoria</a></li>
-    <li><a href="base/language"> Modelos Hospedados do Google Linguagem</a></li>
-    <li><a href="base/sentiment"> Modelos Hospedados do Google Sentimento</a> </li>
-  </ul>
-</div>
+    <h2>Modelos de exemplo do Google</h2>
+    <hr>
+    <div class="row">
+      <div class="col-xs-6 col-sm-3 col-md-2"> <a href="<?php echo site_url('base/category'); ?>" class="btn btn-default">
+        <div class="row"> 
+          <div class="col-xs-12 text-center">
+            <p>Categorias</p>
+          </div>
+        </div>
+        </a> </div>
+      <div class="col-xs-6 col-sm-3 col-md-2"> <a href="<?php echo site_url('base/sentiment'); ?>" class="btn btn-default">
+        <div class="row"> 
+          <div class="col-xs-12 text-center">
+            <p>Sentimento</p>
+          </div>
+        </div>
+        </a> </div>
+        <div class="col-xs-6 col-sm-3 col-md-2"> <a href="<?php echo site_url('base/language'); ?>" class="btn btn-default">
+        <div class="row"> 
+          <div class="col-xs-12 text-center">
+            <p>Linguagem</p>
+          </div>
+        </div>
+        </a> </div>
+    </div>
+    <h2>Modelo do Google modificado</h2>
+    <hr>
+    <div class="row">
+      <div class="col-xs-6 col-sm-3 col-md-4"> <a href="<?php echo site_url('base/coyote_upload'); ?>" class="btn btn-default">
+        <div class="row"> 
+          <div class="col-xs-12 text-center">
+            <p>Enviar arquivo de treino</p>
+          </div>
+        </div>
+        </a> </div>
+      <div class="col-xs-6 col-sm-3 col-md-4"> <a href="<?php echo site_url('base/coyote_status'); ?>" class="btn btn-default">
+        <div class="row"> 
+          <div class="col-xs-12 text-center">
+            <p>Status do treinamento</p>
+          </div>
+        </div>
+        </a> </div>
+        <div class="col-xs-6 col-sm-3 col-md-4"> <a href="<?php echo site_url('base/coyote_predict'); ?>" class="btn btn-default">
+        <div class="row"> 
+          <div class="col-xs-12 text-center">
+            <p>Testar modelo</p>
+          </div>
+        </div>
+        </a> </div>
+    </div>
+     <h2>Modelo Personalizado</h2>
+    <hr>
+    <div class="row">
+      <div class="col-xs-6 col-sm-3 col-md-4"> <a href="<?php echo site_url('coyote/coyote_upload'); ?>" class="btn btn-default">
+        <div class="row"> 
+          <div class="col-xs-12 text-center">
+            <p>Enviar arquivo de treino</p>
+          </div>
+        </div>
+        </a> </div>
+      <div class="col-xs-6 col-sm-3 col-md-4"> <a href="<?php echo site_url('coyote/coyote_status'); ?>" class="btn btn-default">
+        <div class="row"> 
+          <div class="col-xs-12 text-center">
+            <p>Status do treinamento</p>
+          </div>
+        </div>
+        </a> </div>
+        <div class="col-xs-6 col-sm-3 col-md-4"> <a href="<?php echo site_url('coyote/coyote_predict'); ?>" class="btn btn-default">
+        <div class="row"> 
+          <div class="col-xs-12 text-center">
+            <p>Testar modelo</p>
+          </div>
+        </div>
+        </a> </div>
+    </div>
+  </div>
